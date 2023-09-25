35ff7aa079891ecaad45d69d10512a927341fa15
Pavitra Golchha <hello@pavi2410.me>
2023-09-25 15:58:16 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::env;
 // Available if you need it!
 use serde_bencode::{de, value::Value};
 
-fn display(value: &Value) -> String {
+fn to_json(value: &Value) -> serde_json::Value {
     match value {
-        Value::Bytes(bytes) => format!("{:?}", String::from_utf8_lossy(bytes)),
-        Value::Int(num) => format!("{}", num),
-        Value::List(list) => format!("{:?}", list.map(::display)),
-        Value::Dict(dict) => format!("{:?}", dict),
+        Value::Bytes(bytes) => serde_json::value::String(bytes.to_string()),
+        Value::Int(num) => serde_json::value::Number(num),
+        Value::List(list) => serde_json::value::Array(list),
+        Value::Dict(dict) => serde_json::value::Object(dict),
     }
 }
 
@@ -21,7 +21,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value: Value = de::from_str(encoded_value).unwrap();
-        println!("{}", display(&decoded_value));
+        println!("{}", to_json(&decoded_value));
     } else {
         println!("unknown command: {}", args[1])
     }

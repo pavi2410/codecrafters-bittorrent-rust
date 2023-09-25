bd4808f99fb3646206c9565f0a80902521ece18f
Pavitra Golchha <hello@pavi2410.me>
2023-09-25 15:47:39 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use serde_json;
 use std::env;
+use std::fmt;
 
 // Available if you need it!
 use serde_bencode::{de, value::Value};
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::Bytes(bytes) => write!(f, "{:?}", bytes),
+            Value::Int(num) => write!(f, "{}", num),
+            Value::List(list) => write!(f, "{:?}", list),
+            Value::Dict(dict) => write!(f, "{:?}", dict),
+        }
+    }
+}
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {

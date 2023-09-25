90a99d987c9f257d61db88c07650aad2311862e2
Pavitra Golchha <hello@pavi2410.me>
2023-09-26 00:07:12 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ fn main() {
         let torrent = de::from_bytes::<Torrent>(&file_buf).unwrap();
 
         let tracker_options = &[
-            ("info_hash", String::from_utf8(info_hash(&torrent.info)).unwrap()),
+            ("info_hash", String::from_utf8_lossy(info_hash(&torrent.info))),
             ("peer_id", "00112233445566778899".to_string()),
             ("port", "6881".to_string()),
             ("uploaded", "0".to_string()),

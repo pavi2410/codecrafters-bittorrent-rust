67471105ec45e69d27c33bdabb4409b60de54a1e
Pavitra Golchha <hello@pavi2410.me>
2023-09-25 15:36:49 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == "i" { 
+    } else if encoded_value.chars().next().unwrap() == 'i' { 
         let e_index = encoded_value.find("e").unwrap();
         let number_string = &encoded_value[1..e_index];
         let number = number_string.parse::<i64>().unwrap();
